a828aba8ed2cb0a60fc831d618d99a723b85293a
Shamsutdinov Vladislav <okomsky@yandex.ru>
2023-06-29 22:08:38 +1100

    дз готово

---
+++ src/Car.java
@@ -0,0 +1,8 @@
+public class Car extends TransportWithEngine{
+
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+
+}
---
+++ src/Transport.java
@@ -0,0 +1,26 @@
+public abstract class Transport implements CanBeServiced{
+    private final String modelName;
+    private final int wheelsCount;
+
+    public Transport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+    @Override
+    public void service() {
+        for (int i = 0; i < wheelsCount; i++) {
+            updateTyre();
+        }
+    }
+}
